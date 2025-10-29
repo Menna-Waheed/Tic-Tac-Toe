e6d08ef6f2822628cf0cc739b672b89f8e19e45a
menna <mennaa.wahed@gmail.com>
2025-10-29 03:13:50 +0200

    + tic tac toe logic

--- lib/home/home_screen/widget/board_tile.dart
+++ lib/home/home_screen/widget/board_tile.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-
+ typedef OnBoardClick=void Function(int);
 class BoardTile extends StatelessWidget {
-  BoardTile({super.key, required this.symbals});
+  BoardTile({super.key, required this.symbals,required this.onBoardClick,required this.index});
   String symbals;
+  OnBoardClick onBoardClick;
+  int index;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: symbals.isEmpty ? Container() : Image.asset(symbals),
+      child: InkWell(
+          onTap: () {
+           onBoardClick(index);
+          },
+          child: symbals.isEmpty ? Container() : Image.asset('assets/images/$symbals.png')),
     );
   }
 }
